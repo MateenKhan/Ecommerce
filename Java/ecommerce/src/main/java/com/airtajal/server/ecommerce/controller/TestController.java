e668a614ed7ad5ebd7bd5734022593da1d0f26d1
Mateen <makjavaprogrammer@gmail.com>
2025-10-18 19:15:37 +0530

    basic backend spring

---
+++ Java/ecommerce/src/main/java/com/airtajal/server/ecommerce/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.airtajal.server.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "test";
+    }
+    
+}
---
+++ Java/ecommerce/src/test/java/com/airtajal/server/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtajal.server.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
